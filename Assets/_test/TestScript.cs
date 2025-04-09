c23b695986990cb4cf4cfea08498f631af97cf9f
GGemCo <ggemco.master@gmail.com>
2025-04-09 19:09:06 +0900

    [Character] npc 이름 태그 추가 [Addressables] 아이템, npc 이름 태그 프리팹 추가

---
+++ Assets/_test/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace _test
+{
+    public class TestScript : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+        }
+    }
+}
